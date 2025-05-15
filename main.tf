2883388791aae65231eecc463f21c4e6d54063c1
BrijeshKanojiya <64994984+BrijeshKanojiya@users.noreply.github.com>
2025-05-15 19:48:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
 #terraform1
+/*
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.27.0"
+    }
+  }
+}
+*/
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
     }
   }
 }
